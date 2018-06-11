c76a10b14e52b934f0e66c4854f979dee558275d
Alex White <awhit012@gmail.com>
2018-06-11 12:05:36 -0500

    adds plugins and styles editor, post wip

--- src/blog/BlogForm.js
+++ src/blog/BlogForm.js
@@ -1,12 +1,63 @@
 import React, { Component } from 'react';
-import { EditorState, RichUtils } from 'draft-js';
+import { EditorState, RichUtils, convertToRaw } from 'draft-js';
 import Editor from 'draft-js-plugins-editor';
 import createEmojiPlugin from 'draft-js-emoji-plugin';
+import createLinkPlugin from 'draft-js-anchor-plugin';
+import createToolbarPlugin, { Separator } from 'draft-js-static-toolbar-plugin';
+import HeadlinesButton from '../ui-components/HeadlinesButton';
+import {stateToHTML} from 'draft-js-export-html';
+import {
+  ItalicButton,
+  BoldButton,
+  UnderlineButton,
+  CodeButton,
+  HeadlineOneButton,
+  HeadlineTwoButton,
+  HeadlineThreeButton,
+  UnorderedListButton,
+  OrderedListButton,
+  BlockquoteButton,
+  CodeBlockButton,
+} from 'draft-js-buttons';
 import './Blog.css';
 import 'draft-js-emoji-plugin/lib/plugin.css';
+import 'draft-js-static-toolbar-plugin/lib/plugin.css';
+import Button from '@material-ui/core/Button';
 
 const emojiPlugin = createEmojiPlugin();
 const { EmojiSuggestions } = emojiPlugin;
+const linkPlugin = createLinkPlugin();
+const toolbarPlugin = createToolbarPlugin({
+  structure: [
+    BoldButton,
+    ItalicButton,
+    UnderlineButton,
+    CodeButton,
+    Separator,
+    HeadlinesButton,
+    UnorderedListButton,
+    BlockquoteButton,
+    linkPlugin.LinkButton
+  ]
+});
+
+const { Toolbar } = toolbarPlugin;
+const plugins = [emojiPlugin, toolbarPlugin, linkPlugin];
+
+let options = {
+  inlineStyles: {
+    // Override default element (`strong`).
+    BOLD: {element: 'b'},
+    ITALIC: {
+      // Add custom attributes. You can also use React-style `className`.
+      attributes: {class: 'foo'},
+      // Use camel-case. Units (`px`) will be added where necessary.
+      style: {fontSize: 12}
+    },
+    // Use a custom inline style. Default element is `span`.
+    RED: {style: {color: '#900'}},
+  },
+};
 
 
 class BlogForm extends Component {
@@ -17,6 +68,8 @@ class BlogForm extends Component {
     };
     this.onChange = this.onChange.bind(this);
     this.handleKeyCommand = this.onChange.bind(this);
+    this.focus = this.focus.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   onChange(editorState) {
@@ -32,17 +85,35 @@ class BlogForm extends Component {
     return 'not-handled';
   }
 
+  focus() {
+    this.editor.focus();
+  };
+
+  handleSubmit() {
+    const contentState = this.state.editorState.getCurrentContent();
+    // console.log('content state', convertToRaw(contentState));
+    let html = stateToHTML(contentState, options);
+    console.log(html)
+
+  }
+
   render() {
     return (
     	<div>
     		<h1>Hey Beautiful! Look at you bloggin!</h1>
-	      <Editor 
-	      	id="text-editor"
-	      	editorState={this.state.editorState}
-	      	handleKeyCommand={this.handleKeyCommand}
-	      	onChange={this.onChange}
-	      	plugins={[emojiPlugin]}/>
-	      	<EmojiSuggestions />
+    		<div className="text-editor"  onClick={this.focus}>
+	      	<Editor 
+		      	editorState={this.state.editorState}
+		      	handleKeyCommand={this.handleKeyCommand}
+		      	onChange={this.onChange}
+		      	ref={(element) => { this.editor = element; }}
+		      	plugins={plugins}/>
+		      	<EmojiSuggestions />
+		      	<div className="toolbar">
+	          	<Toolbar />
+		      	</div>
+    		</div>
+    		<Button onClick={this.handleSubmit}>Post</Button>
     	</div>
     );
   }
