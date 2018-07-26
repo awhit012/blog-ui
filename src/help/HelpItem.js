ca3e235ad894c401b9b88292ae120e943f0d1ce8
Alex White <awhit012@gmail.com>
2018-07-26 14:13:57 -0500

    adds application to 1:1

--- src/help/HelpItem.js
+++ src/help/HelpItem.js
@@ -12,7 +12,7 @@ class HelpItem extends Component {
           initialImage="/images/loading_icon.gif"
           fallbackImage="/images/just-logo.png"/>
         <div className="help-right">
-          <h1><a href={this.props.item.url}>{this.props.item.name}</a></h1>
+          <h1><a href={this.props.item.url} target="_blank">{this.props.item.name}</a></h1>
           <p>{this.props.item.description}</p> 
           {this.props.item.id !== 1984 ? <h4>My Take</h4> : ""} 
           <p>{this.props.item.myTake}</p>
