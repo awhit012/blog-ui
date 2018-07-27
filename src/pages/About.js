8d87923410560f4acd595020f53599131e7ccab1
Alex White <awhit012@gmail.com>
2018-07-27 09:55:46 -0500

    spellcheck

--- src/pages/About.js
+++ src/pages/About.js
@@ -20,7 +20,7 @@ class About extends Component {
 			I'm not a medical expert, I'm a teacher, web developer, traveler, writer and husband. I got diagnosed with Fibromyalgia a few years ago, but have been battling severe muscle tightness and pain, digestive issues, and chronic fatigue since I was a child. 
 	  	</p>
 		<p className="container">
-	    	Although I'm still on my healing journey, I've learned a ton in the process, especially since I became a patient at <a href="https://www.drmorsesherbalhealthclub.com/">Dr. Robert Morse's</a> clinic. His approach to health and the human body has been a complete gamechanger for me. Between his teachings, and everything I learned in the years before, I have a wealth of knowledge I want to share with you. Given my own completely unhelpful experiences with a traditional doctor, I find the mission of sharing this information to be quite urgent. 
+	    	Although I'm still on my healing journey, I've learned a ton in the process, especially since I became a patient at <a href="https://www.drmorsesherbalhealthclub.com/">Dr. Robert Morse's</a> clinic. His approach to health and the human body has been a complete game-changer for me. Between his teachings, and everything I learned in the years before, I have a wealth of knowledge I want to share with you. Given my own completely unhelpful experiences with a traditional doctor, I find the mission of sharing this information to be quite urgent. 
 	  	</p>
 		<p className="container">
 	    	Today as I write this, I feel SO MUCH BETTER than I did just a year ago. This site is dedicated to sharing information and resources that has helped me personally with anyone else who is suffering from Fibromyalgia, Chronic Fatigue, Chronic Pain, LBS, IBS, or even people like me who have very limiting dietary restrictions. If I can help just a couple of people improve their quality of life, that's a huge win!
@@ -38,11 +38,11 @@ class About extends Component {
 		<h2>How it works</h2>
 		
 		<p className="container">
-			This site is, and always will be, free and open to the public. To pay the bills and support what I'm doing, I use <a href="https://affiliate-program.amazon.com/">Amazon Affiliate Marketing</a> links. If you click through a link on our site, and buy something on Amazon.com, you pay the same low price as always, and we recieve a modest commision for bringing customers to Amazon! 
+			This site is, and always will be, free and open to the public. To pay the bills and support what I'm doing, I use <a href="https://affiliate-program.amazon.com/">Amazon Affiliate Marketing</a> links. If you click through a link on our site, and buy something on Amazon.com, you pay the same low price as always, and we receive a modest commission for bringing customers to Amazon! 
 		</p>
 
 		<p className="container jumbo-bottom">
-			Because I know what it is to live with Fibromyalgia, it is important to me that this site not just provide free, quality information, but also give back monetarily. That's why I donate 5% of our commisions to <a href="http://www.afsafund.org/">The American Fibromyalgia Syndrome Association</a>.
+			Because I know what it is to live with Fibromyalgia, it is important to me that this site not just provide free, quality information, but also give back monetarily. That's why I donate 5% of our commissions to <a href="http://www.afsafund.org/">The American Fibromyalgia Syndrome Association</a>.
 		</p>
     </div>	
     
